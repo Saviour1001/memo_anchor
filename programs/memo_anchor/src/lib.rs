6da923741c5ec8a011cc7e8c459205c9082f694c
Harsh Ghodkar <harshghodkar@gmail.com>
2022-03-08 01:14:21 +0530

    updates on the rust

--- programs/memo_anchor/src/lib.rs
+++ programs/memo_anchor/src/lib.rs
@@ -6,17 +6,15 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod memo_anchor {
     use super::*;
     use std::str;
-    pub fn build_memo(ctx: Context<Initialize>, input:&[u8]) -> Result<()> {
+    pub fn build_memo(ctx: Context<BuildMemo>, input:Vec<u8>) -> Result<()> {
 
         msg!("Initializing");
-        // let account = &mut ctx.accounts.my_account;
-        // let account_info_iter = account.iter();
-        
-        let account_info_iter = [1,2,3,4];
+
+        let accounts = ctx.remaining_accounts;
 
         let mut missing_required_signature = false;
-        for account_info in account_info_iter.iter() {
-            if let Some(address) = account_info.address() {
+        for account_info in accounts.iter() {
+            if let Some(address) = account_info.signer_key() {
                 msg!("Signed by: {:?}", address);
             }
             else{
@@ -27,19 +25,16 @@ pub mod memo_anchor {
             return err!(MyError::MissingRequiredSignature);
         }
 
-        // let memo = input.map_err(|err| {
-        //     msg!("Invalid UTF-8, from bytes: {:?}", err);
-        // })?;
 
-        let memo = str::from_utf8(&input).map_err(|err| {
+        let unwrapped_input = str::from_utf8(&input).map_err(|err| {
             msg!("Invalid UTF-8, from bytes: {:?}", err);
         });
 
-        msg!("Memo: {:?}", memo);
-
-        // msg!("Memo (len {}): {:?}", memo.len(), memo);
-        
-
+    //     let memo = str::from_utf8(input).map_err(|err| {
+    //     msg!("Invalid UTF-8, from byte {}", err.valid_up_to());
+    //     ProgramError::InvalidInstructionData
+    // })?;
+    msg!("Memo (len {}): {:?}", unwrapped_input.unwrap().len() , unwrapped_input);
 
         Ok(())
     }
@@ -47,21 +42,16 @@ pub mod memo_anchor {
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
-    pub my_account: Account<'info, MyMemo>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+pub struct BuildMemo<> {
+
 }
 
 #[error_code]
 pub enum MyError {
     #[msg("Missing required signature")]
-    MissingRequiredSignature
+    MissingRequiredSignature,
+    #[msg("Invalid Instruction Data")]
+    InvalidInstructionData
 }
 
-#[account]
-pub struct MyMemo {
-    pub input: u8,
-}
+
